8261eae2ee19a959b5389ff229ef43b975e6adc0
luiz.bezerra <luizantoniopcb@gmail.com>
2025-01-09 00:06:38 -0300

    created concepts service

--- conceitos-nest/src/concepts/concepts.controller.ts
+++ conceitos-nest/src/concepts/concepts.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConceptsService } from './concepts.service';
 
 @Controller('concepts')
 export class ConceptsController {
+  constructor(private readonly conceptsService: ConceptsService) {}
+
   @Get()
   getConcepts(): string {
-    return 'Controller Concepts';
+    return this.conceptsService.getConcepts();
   }
 }
