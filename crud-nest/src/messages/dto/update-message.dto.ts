9a2baeae12d2e10d0ef344f32d7b096a34d08b53
luiz.bezerra <luizantoniopcb@gmail.com>
2025-01-11 00:38:18 -0300

    instace ValidatorPipe and validate data in create-message.dto

--- crud-nest/src/messages/dto/update-message.dto.ts
+++ crud-nest/src/messages/dto/update-message.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, MaxLength } from 'class-validator';
+
 export class UpdateMessageDto {
+  @IsString()
+  @MaxLength(255)
   readonly text?: string;
+
+  @IsString()
   readonly from?: string;
+
+  @IsString()
   readonly to?: string;
 }
